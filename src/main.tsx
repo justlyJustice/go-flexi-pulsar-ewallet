4255c8fdea6c7031a67e35115327310ebcf515b1
unknown <justlyjohn198@gmail.com>
2025-07-27 15:29:35 +0100

    Added log

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,24 @@ import App from "./App";
 import "./index.css";
 
 Sentry.init({
-  dsn: "https://6386584af50015b0d6c4bc8cd6181b83@o518596.ingest.us.sentry.io/4509740855525376",
+  dsn: "https://e4f4efa6004d80507d8c4a8a30a6a71a@o518596.ingest.us.sentry.io/4509740953436160",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // Logs
+  enableLogs: true,
 });
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
